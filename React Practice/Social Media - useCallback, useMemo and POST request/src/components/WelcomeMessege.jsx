593f7844cbe603c712f37e3f9890538f592690d5
Parth Vighne <parthvighne@gmail.com>
2024-01-31 16:01:35 +0530

    useMemo and useCallBack

---
+++ React Practice/Social Media - useCallback, useMemo and POST request/src/components/WelcomeMessege.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+const WelcomeMessege = () => {
+  return (
+    <center className="welcome-messege">
+      <h1>There are no posts</h1>
+      {/* <button type="button" className="btn btn-primary">
+        Get Posts from Server
+      </button> */}
+    </center>
+  );
+};
+
+export default WelcomeMessege;
